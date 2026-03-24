d1607d3a7d64d1e071469e9752dbe7513d483825
Morar-AC <andreicristian0812@gmail.com>
2026-03-24 19:19:58 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,63 +4,146 @@ using namespace std;
 
 
 
-
-
+int n;
+int a[100001];
 
 stack<pair<int,int>>s;
+
+int st[100001],dr[100001];
+
+
+
+
+
 
 
 
 int main()
 {
 
-      int n;
-      cin >> n;
-      int a[100001];
-      for(int i = 1;i<=n;++i)
-         cin >> a[i];
+    cin >> n;
+    for(int i = 1;i<=n;++i)
+       cin >> a[i];
+
+
+
+           for(int i = 1;i<=n;++i)
+           {
+                if(i == 1)
+                    st[i] = i;
+                    else{
+
+                         if(a[i-1] < a[i])
+                              st[i] = i;
+
+                        else{
+
+                              while(!s.empty())
+                                 {
+                                     int val = s.top().first;
+                                     if(val >= a[i])
+                                          s.pop();
+                                          else break;
+
+
+                                 }
+
+
+                               if(s.size())
+                                st[i] = s.top().second + 1;
+                                else st[i] = 1;
 
 
 
 
-         for(int i = 1;i<=n;++i)
-            {
-                if(i != 1)
-                  {
-                      if(a[i] > a[i-1])
-                         cout << "pt pozitia " << i << " " << "index corect e: " << i-1 << endl;
-                      else{
-
-                        while(!s.empty())
-                          {
-                              int val =s.top().first;
-                              if(val >= a[i])
-                                 s.pop();
-                                 else break;
 
 
 
 
+                        }
 
 
-                          }
-
-                          if(s.size())
-                         cout << "pt pozitia " << i << " " << "index corect e: " << s.top().second << endl;
-
-                      }
-
-
-
-
-
-                  }
-
+                    }
 
 
                 s.push({a[i],i});
 
-            }
+
+           }
+
+
+            while(s.size())
+             s.pop();
+
+
+
+               for(int i = n;i>=1;--i)
+           {
+                if(i == n)
+                    dr[i] = n;
+                    else{
+
+                         if(a[i+1] < a[i])
+                              dr[i] = i;
+
+                        else{
+
+                              while(!s.empty())
+                                 {
+                                     int val = s.top().first;
+                                     if(val >= a[i])
+                                          s.pop();
+                                          else break;
+
+
+                                 }
+
+
+                               if(s.size())
+                                dr[i] = s.top().second - 1;
+                                else dr[i] = n;
+
+
+
+
+
+
+
+
+                        }
+
+
+                    }
+
+
+                s.push({a[i],i});
+
+
+           }
+
+
+
+
+             int ans = 0;
+             for(int i = 1;i<=n;++i)
+               {
+                   int k = abs(st[i]-dr[i]) + 1;
+                   k*=a[i];
+                   ans = max(ans,k);
+
+
+
+               }
+
+
+
+
+
+                cout << ans;
+
+
+
+
 
 
 
