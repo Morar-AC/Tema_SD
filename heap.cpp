df0bf8aabd7099a7ab78cc6cb83084b08d2e8ea9
Morar-AC <andreicristian0812@gmail.com>
2026-05-12 10:04:00 +0300

    Add files via upload

---
+++ heap.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+
+
+priority_queue<int,vector<int>,greater<int>>minhp;
+
+priority_queue<int>maxhp;
+
+
+void insert(int x) {
+
+    maxhp.push(x);
+
+
+    minhp.push(maxhp.top());
+    maxhp.pop();
+
+
+    if (maxhp.size() < minhp.size()) {
+        maxhp.push(minhp.top());
+        minhp.pop();
+    }
+}
+int getMedian() {
+    if (maxhp.size() > minhp.size()) {
+
+
+        return maxhp.top();
+
+    } else {
+
+        return (maxhp.top()+minhp.top())/2;
+    }
+}
+
+int main()
+{
+
+    insert(1);
+    insert(4);
+    insert(3);
+    cout << getMedian();
+
+
+
+
+}
